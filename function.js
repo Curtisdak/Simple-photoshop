8d55c8f04f86c7312fc5d999cdcab71956136d58
Curtisdak <curtis.dakouri@gmail.com>
2025-03-11 03:48:21 +0100

    I'm done with the toggle button for the dark mode

--- function.js
+++ function.js
@@ -1,3 +1,39 @@
+const htmlElement = document.documentElement;
+const sunBtn = document.querySelector(".sun");
+const moonBtn = document.querySelector(".moon");
+moonBtn.style.display = "none";
+
+
 function toggleMode() {
     console.log("hello")
+
+
+    htmlElement.classList.toggle("dark")
+    if (htmlElement.classList.contains("dark")) {
+        sunBtn.style.display = "none";
+        moonBtn.style.display = "block";
+        localStorage.setItem("theme", "dark")
+    } else {
+        sunBtn.style.display = "block";
+        moonBtn.style.display = "none";
+        localStorage.setItem("theme", "")
+    }
+
+
 }
+
+function loadThemes() {
+    const savedTheme = localStorage.getItem("theme")
+
+    if (savedTheme === "dark") {
+        htmlElement.classList.add("dark")
+        sunBtn.style.display = "none";
+        moonBtn.style.display = "block";
+    } else {
+        htmlElement.classList.remove("dark")
+        sunBtn.style.display = "block";
+        moonBtn.style.display = "none";
+    }
+}
+
+loadThemes()
